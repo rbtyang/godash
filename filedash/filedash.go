d0924a6f6a64efd76a97c500640461e0ef308a13
robotyang <robotyang123@163.com>
2022-04-06 23:46:12 +0800

    add strdash, add filedash func

--- filedash/filedash.go
+++ filedash/filedash.go
@@ -1,45 +1,114 @@
 package filedash
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"os"
-	"fmt"
+	"path/filepath"
+	"strings"
 )
 
-/*
- 判断文件或文件夹是否存在；
- 如果返回的错误为nil,说明文件或文件夹存在；
- 如果返回的错误类型使用os.IsNotExist()判断为true,说明文件或文件夹不存在；
- 如果返回的错误为其它类型,则不确定是否在存在；
-*/
-func PathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
+// 判断 所给路径 是否为文件夹
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
 	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
+	return s.IsDir()
 }
 
-/*
- 从一个文件拷贝到另一个文件；
-*/
-func CopyFile(dstName, srcName string) (writeen int64, err error) {
-	src, err := os.Open(dstName)
+// 判断 所给路径 是否为文件
+func IsFile(path string) bool {
+	return !IsDir(path)
+}
+
+// 判断 文件或文件夹 是否存在；
+// 如果返回的 错误 为nil,说明文件或文件夹存在；
+// 如果返回的 错误类型 使用os.IsNotExist() 判断为true, 说明 文件或文件夹 不存在；
+// 如果返回的 错误 为其它类型, 则不确定 是否在存在；
+func IsExist(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
+}
+
+// 判断 文件 是否存在；
+func IsExistFile(filePath string) bool {
+	return IsFile(filePath) && IsExist(filePath)
+}
+
+// 判断 文件夹 是否存在；
+func IsExistDir(dirPath string) bool {
+	return IsDir(dirPath) && IsExist(dirPath)
+}
+
+func Create(filePath string) (*os.File, error) {
+	dirPath, _ := filepath.Split(filePath)
+	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
+		return nil, err
+	}
+	return os.Create(filePath)
+}
+
+func CreateIfNotExist(filePath string) (*os.File, error) {
+	dirPath, _ := filepath.Split(filePath)
+	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	if IsExist(filePath) {
+		return os.OpenFile(filePath, os.O_RDWR, os.ModePerm)
+	} else {
+		return os.Create(filePath)
+	}
+}
+
+// 从一个文件 拷贝到 另一个文件
+func CopyFile(srcFilePath, dstFilePath string) (writeen int64, err error) {
+	//打开 原文件
+	srcFile, err := os.Open(srcFilePath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer src.Close()
+	defer srcFile.Close()
 
-	dst, err := os.OpenFile(srcName, os.O_CREATE|os.O_WRONLY, 0644)
+	//打开或创建 目标文件
+	dstFile, err := os.OpenFile(dstFilePath, os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer dst.Close()
+	defer dstFile.Close()
 
-	return io.Copy(dst, src)
+	//复制 文件
+	return io.Copy(dstFile, srcFile)
+}
+
+// 获取 最后一个 目录名
+func GetLastDir(path string) string {
+	sep := string(os.PathSeparator)
+	path, _ = filepath.Abs(path)
+	dir, _ := filepath.Split(path)
+	dir = strings.TrimRight(dir, "\\/")      //去掉最后的 /
+	dir = strings.ReplaceAll(dir, "\\", sep) //转换所有的 /
+	dirArr := strings.Split(dir, sep)        //根据 / 进行分割
+	lastDir := dirArr[len(dirArr)-1]         //获取 最后一个 目录名
+	return lastDir
+}
+
+func GetLastDirWithCheck(path string) (string, error) {
+	if !IsExist(path) || !IsDir(path) {
+		return "", errors.New(path + "非有效目录路径")
+	}
+	return GetLastDir(path), nil
 }
---
+++ strdash/strdash.go
@@ -0,0 +1,100 @@
+package strdash
+
+import (
+	"strings"
+)
+
+// 首字母大写
+func UpperFirst(s string) string {
+	if s == "" {
+		return s
+	}
+	return strings.ToUpper(string([]rune(s)[0])) + string([]rune(s)[1:])
+}
+
+/*
+ 驼峰转蛇形 snake string
+ @description XxYy to xx_yy , XxYY to xx_y_y
+ @param s 需要转换的字符串
+ @return string
+*/
+func SnakeString(s string) string {
+	return snakeStr(s, '_')
+}
+
+/*
+ 驼峰转蛇形 snake string
+ @description XxYy to xx-yy , XxYY to xx-y-y
+ @param s 需要转换的字符串
+ @return string
+*/
+func SnakeStringStrike(s string) string {
+	return snakeStr(s, '-')
+}
+
+// 通用转换
+func snakeStr(s string, sep byte) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		// or通过ASCII码进行大小写的转化
+		// 65-90（A-Z），97-122（a-z）
+		//判断如果字母为大写的A-Z就在前面拼接一个_
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, sep)
+		}
+		if d != sep {
+			j = true
+		}
+		data = append(data, d)
+	}
+	//ToLower把大写字母统一转小写
+	return strings.ToLower(string(data[:]))
+}
+
+/*
+ 蛇形转驼峰
+ @description xx_yy to XxYx  xx_y_y to XxYY
+ @param s要转换的字符串
+ @return string
+*/
+func CamelStringStrike(s string) string {
+	return camelStr(s, '-')
+}
+
+/*
+ 蛇形转驼峰
+ @description xx-yy to XxYx  xx-y-y to XxYY
+ @param s要转换的字符串
+ @return string
+*/
+func CamelString(s string) string {
+	return camelStr(s, '_')
+}
+
+// 通用转换
+func camelStr(s string, sep byte) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == sep && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}

520ce0044fc8856e42e115febb02e393df8c3cf4
robotyang <robotyang123@163.com>
2024-06-27 18:07:46 +0800

    docs(所有包): 补充用例
    - 补充 其他包用例

--- dashjson/json_test.go
+++ dashjson/json_test.go
@@ -14,18 +14,19 @@ type Student struct {
 	Level     int32    `comment:"评级" remark:"【必填】0~10级"`
 	Gender    int32    `comment:"性别" remark:"【可选】0男，1女，默认0"`
 	Mobile    string   `comment:"号码" remark:"【可选】年段为高中生时，则联系号码必填"`
-	ClassUuid string   `comment:"班级uuid" remark:"【可选】"`
-	ClassName string   `comment:"班级名称" remark:"【可选】班级uuid不为空时，则班级名称必填"`
+	ClassUuid string   `json:"class_uuid" comment:"班级uuid" remark:"【可选】"`
+	ClassName string   `json:"class_name" comment:"班级名称" remark:"【可选】班级uuid不为空时，则班级名称必填"`
 	Hobbies   []string `comment:"兴趣" remark:"【可选】1唱 2跳 3rap 4篮球"`
 	Address   *Address `comment:"住所地址" remark:"【可选】默认空"`
 	Remark    string   `comment:"备注" remark:"【可选】默认空"`
+	Ext       any      `commont:"其他"`
 }
 type Address struct {
 	Name   string `comment:"地址别名" remark:"【可选】默认空"`
 	Detail string `comment:"详细地址" remark:"【可选】默认空"`
 }
 
-var student = Student{
+var _student = Student{
 	Uuid:      "a223db68-0ea4-11ef-8f30-fa163e930d3e",
 	Type:      "MIDDLE",
 	Name:      "张三",
@@ -45,19 +46,19 @@ var student = Student{
 /*
 @Editor robotyang at 2024
 
-TestEmpty is a ...
+TestMarshal is a ...
 */
 func TestMarshal(t *testing.T) {
 	cases := []any{
 		``,
 		`a`,
-		student,
+		_student,
 	}
 
-	student2 := student
-	student2.Hobbies = nil
-	student2.Address = nil
-	cases = append(cases, student2)
+	stu2 := _student
+	stu2.Hobbies = nil
+	stu2.Address = nil
+	cases = append(cases, stu2)
 
 	for _, cs := range cases {
 		byt, err := json.Marshal(cs)
@@ -71,6 +72,103 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
-func TestMarshalWithTag(t *testing.T) {
-	//TODO here
+/*
+@Editor robotyang at 2024
+
+TestWithTag is a ...
+*/
+func TestWithTag(t *testing.T) {
+	stu1 := _student
+	want := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":null}`
+	recv, err := dashjson.Marshal(stu1)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, want, recv)
+
+	stu2 := &Student{}
+	err = dashjson.Unmarshal(recv, stu2)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "a223db68-0ea4-11ef-8f30-fa163e930d3e", stu2.Uuid)
+	assert.Equal(t, "a223db68-0ea4-11ef-8f30", stu2.ClassUuid)
+	assert.Equal(t, "三年一班", stu2.ClassName)
+}
+
+/*
+@Editor robotyang at 2024
+
+TestWithAnyData is a ...
+*/
+func TestWithAnyData(t *testing.T) {
+	stu := &Student{}
+	{
+		data := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":null}`
+		{
+			err := json.Unmarshal([]byte(data), stu)
+			assert.Equal(t, nil, err)
+		}
+		{
+			err := dashjson.Unmarshal(data, &stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, nil, stu.Ext)
+		}
+	}
+	{
+		data := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":123}`
+		{
+			err := json.Unmarshal([]byte(data), stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, float64(123), stu.Ext)
+		}
+		{
+			err := dashjson.Unmarshal(data, &stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, float64(123), stu.Ext)
+		}
+	}
+	{
+		data := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":"abc"}`
+		{
+			err := json.Unmarshal([]byte(data), stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, "abc", stu.Ext)
+		}
+		{
+			err := dashjson.Unmarshal(data, &stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, "abc", stu.Ext)
+		}
+	}
+	{
+		data := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":[123,456]}`
+		{
+			err := json.Unmarshal([]byte(data), stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, 2, len(stu.Ext.([]interface{})))
+		}
+		{
+			err := dashjson.Unmarshal(data, &stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, 2, len(stu.Ext.([]interface{})))
+		}
+	}
+	{
+		data := `{"Uuid":"a223db68-0ea4-11ef-8f30-fa163e930d3e","Type":"MIDDLE","Name":"张三","Level":9,"Gender":1,"Mobile":"1234567890","class_uuid":"a223db68-0ea4-11ef-8f30","class_name":"三年一班","Hobbies":["篮球","足球","排球"],"Address":{"Name":"三里屯","Detail":"北京市朝阳区三里屯"},"Remark":"三好学生","Ext":[123,"abc"]}`
+		{
+			err := json.Unmarshal([]byte(data), stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, 2, len(stu.Ext.([]interface{})))
+			if ext, ok := stu.Ext.([]interface{}); ok {
+				assert.Equal(t, float64(123), ext[0])
+				assert.Equal(t, "abc", ext[1])
+			}
+		}
+		{
+			err := dashjson.Unmarshal(data, &stu)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, 2, len(stu.Ext.([]interface{})))
+			if ext, ok := stu.Ext.([]interface{}); ok {
+				assert.Equal(t, float64(123), ext[0])
+				assert.Equal(t, "abc", ext[1])
+			}
+		}
+	}
 }
---
+++ dashrun/internal/runtest/runtest.go
@@ -0,0 +1,40 @@
+package runtest
+
+import (
+	"github.com/rbtyang/godash/dashrun"
+	"github.com/stretchr/testify/assert"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestLastCallerPlace(t *testing.T, data []string) []string {
+	data = append(data, "11:14")
+	data = testLastCallerPlace1(t, data)
+	data = append(data, "11:16")
+	return data
+}
+
+func testLastCallerPlace1(t *testing.T, data []string) []string {
+	data = append(data, "11:14")
+	data = testLastCallerPlace2(t, data)
+	data = append(data, "11:16")
+	return data
+}
+
+func testLastCallerPlace2(t *testing.T, data []string) []string {
+	data = append(data, "22:19")
+	data = append(data, "22:28")
+
+	lst := dashrun.LastCallerFuncName(true)
+	assert.Equal(t, "runtest.testLastCallerPlace1", lst)
+	lstFull := dashrun.LastCallerFuncName(false)
+	assert.Equal(t, "github.com/rbtyang/godash/dashrun/internal/runtest.testLastCallerPlace1", lstFull)
+
+	_, curFileName, _, _ := runtime.Caller(0)
+	lstPath := dashrun.LastCallerPlace(curFileName)
+	bol := strings.Contains(lstPath, "godash/dashrun/run_test.go")
+	assert.Equal(t, true, bol)
+
+	return data
+}
